d4aa2fb33e4720143f83d96e59baca0fa8583742
vmarni <vmarni@interos.net>
2019-10-16 15:31:27 -0400

    adding graphs and minor things

--- mac-config/setup.sh
+++ mac-config/setup.sh
@@ -1,3 +1,19 @@
 #/bin/bash!
 
-ln emacs ~/.emacs
+# install homebrew #
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# brew install required libs
+brew install git
+brew install wget
+brew cask install emacs
+brew install tree
+brew install gettext
+brew link --force gettext
+
+# clone reqiured git repos
+git clone https://github.com/narayana1043/notes.git
+
+# hard link files
+ln notes/home-config/emacs ~/.emacs
